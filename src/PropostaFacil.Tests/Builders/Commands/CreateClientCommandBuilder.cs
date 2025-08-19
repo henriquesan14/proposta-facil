64347835edcce1a207e8db41f2bed0880315dae2
Henrique <henriqueunipe@gmail.com>
2025-08-19 16:43:48 -0300

    create client teste

---
+++ src/PropostaFacil.Tests/Builders/Commands/CreateClientCommandBuilder.cs
@@ -0,0 +1,109 @@
+﻿using PropostaFacil.Application.Clients.Commands.CreateClient;
+
+namespace PropostaFacil.Tests.Builders.Commands
+{
+    public class CreateClientCommandBuilder
+    {
+        private string _name = "Default Client";
+        private string _document = "12345678901";
+        private string _email = "client@example.com";
+        private string _phoneNumber = "11999999999";
+        private string _addressStreet = "Street";
+        private string _addressNumber = "123";
+        private string _addressComplement = "ap1";
+        private string _addressDistrict = "district";
+        private string _addressCity = "city";
+        private string _addressState = "ST";
+        private string _addressZipCode = "12345000";
+        private Guid? _tenantId = Guid.NewGuid();
+
+        public CreateClientCommandBuilder WithName(string name)
+        {
+            _name = name;
+            return this;
+        }
+
+        public CreateClientCommandBuilder WithDocument(string document)
+        {
+            _document = document;
+            return this;
+        }
+
+        public CreateClientCommandBuilder WithEmail(string email)
+        {
+            _email = email;
+            return this;
+        }
+
+        public CreateClientCommandBuilder WithPhoneNumber(string phoneNumber)
+        {
+            _phoneNumber = phoneNumber;
+            return this;
+        }
+
+        public CreateClientCommandBuilder WithAddressStreet(string street)
+        {
+            _addressStreet = street;
+            return this;
+        }
+
+        public CreateClientCommandBuilder WithAddressNumber(string number)
+        {
+            _addressNumber = number;
+            return this;
+        }
+
+        public CreateClientCommandBuilder WithAddressComplement(string complement)
+        {
+            _addressComplement = complement;
+            return this;
+        }
+
+        public CreateClientCommandBuilder WithAddressDistrict(string district)
+        {
+            _addressDistrict = district;
+            return this;
+        }
+
+        public CreateClientCommandBuilder WithAddressCity(string city)
+        {
+            _addressCity = city;
+            return this;
+        }
+
+        public CreateClientCommandBuilder WithAddressState(string state)
+        {
+            _addressState = state;
+            return this;
+        }
+
+        public CreateClientCommandBuilder WithAddressZipCode(string zipCode)
+        {
+            _addressZipCode = zipCode;
+            return this;
+        }
+
+        public CreateClientCommandBuilder WithTenantId(Guid? tenantId)
+        {
+            _tenantId = tenantId;
+            return this;
+        }
+
+        public CreateClientCommand Build() =>
+            new CreateClientCommand(
+                _name,
+                _document,
+                _email,
+                _phoneNumber,
+                _addressStreet,
+                _addressNumber,
+                _addressComplement,
+                _addressDistrict,
+                _addressCity,
+                _addressState,
+                _addressZipCode,
+                _tenantId
+            );
+    }
+
+}
---
+++ src/PropostaFacil.Tests/Commands/CreateClientCommandTests.cs
@@ -0,0 +1,129 @@
+﻿using Moq;
+using PropostaFacil.Application.Clients.Commands.CreateClient;
+using PropostaFacil.Application.Shared.Interfaces;
+using PropostaFacil.Domain.Entities;
+using PropostaFacil.Domain.Enums;
+using PropostaFacil.Domain.ValueObjects.Ids;
+using PropostaFacil.Tests.Builders.Commands;
+using PropostaFacil.Tests.Builders.Entities;
+using System.Linq.Expressions;
+
+namespace PropostaFacil.Tests.Commands
+{
+    public class CreateClientCommandTests
+    {
+        private readonly Mock<IUnitOfWork> _unitOfWorkMock = new();
+        private readonly Mock<ICurrentUserService> _currentUserServiceMock = new();
+
+        private CreateClientCommandHandler CreateHandler()
+        => new CreateClientCommandHandler(_unitOfWorkMock.Object, _currentUserServiceMock.Object);
+
+        [Fact]
+        public async Task Handle_Should_Return_TenantRequired_When_AdminSystem_Without_TenantId()
+        {
+            // Arrange
+            var command = new CreateClientCommandBuilder()
+                .WithTenantId(null)
+                .Build();
+
+            _currentUserServiceMock.Setup(x => x.Role).Returns(UserRoleEnum.AdminSystem);
+
+            var handler = CreateHandler();
+
+            // Act
+            var result = await handler.Handle(command, CancellationToken.None);
+
+            // Assert
+            Assert.False(result.IsSuccess);
+            Assert.Equal("Tenants.Validation", result.Error!.Code);
+        }
+
+        [Fact]
+        public async Task Handle_Should_Return_NotFound_When_AdminSystem_Passes_Invalid_Tenant()
+        {
+            // Arrange
+            var command = new CreateClientCommandBuilder()
+                .Build();
+
+            _currentUserServiceMock.Setup(x => x.Role).Returns(UserRoleEnum.AdminSystem);
+            _unitOfWorkMock.Setup(x => x.Tenants.GetByIdAsync(
+                It.IsAny<TenantId>(),
+                It.IsAny<bool>(),
+                It.IsAny<List<Expression<Func<Tenant, object>>>>()
+                )
+            )
+                .ReturnsAsync((Tenant?)null);
+
+            var handler = CreateHandler();
+
+            // Act
+            var result = await handler.Handle(command, CancellationToken.None);
+
+            // Assert
+            Assert.False(result.IsSuccess);
+            Assert.Equal("Tenants.NotFound", result.Error!.Code);
+        }
+
+        [Fact]
+        public async Task Handle_Should_Return_Conflict_When_Document_Already_Exist()
+        {
+            // Arrange
+            var command = new CreateClientCommandBuilder()
+                .Build();
+
+            var tenant = new TenantBuilder()
+                .Build();
+
+            var client = new ClientBuilder()
+                .Build();
+
+            _currentUserServiceMock.Setup(x => x.Role).Returns(UserRoleEnum.AdminSystem);
+
+            _unitOfWorkMock.Setup(x => x.Tenants.GetByIdAsync(It.IsAny<TenantId>(), false, null!))
+                .ReturnsAsync(tenant);
+
+            _unitOfWorkMock
+                .Setup(x => x.Clients.GetSingleAsync(It.IsAny<Expression<Func<Client, bool>>>(), false, null!))
+                .ReturnsAsync(client);
+
+            var handler = CreateHandler();
+
+            // Act
+            var result = await handler.Handle(command, CancellationToken.None);
+
+            // Assert
+            Assert.False(result.IsSuccess);
+            Assert.Equal("Clients.Conflict", result.Error!.Code);
+        }
+
+        [Fact]
+        public async Task Handle_Should_Create_Client_When_Data_Is_Valid()
+        {
+            // Arrange
+            var command = new CreateClientCommandBuilder()
+                .Build();
+
+            var tenant = new TenantBuilder()
+                .Build();
+
+            _currentUserServiceMock.Setup(x => x.Role).Returns(UserRoleEnum.AdminSystem);
+
+            _unitOfWorkMock.Setup(x => x.Tenants.GetByIdAsync(It.IsAny<TenantId>(), false, null!))
+                .ReturnsAsync(tenant);
+
+            _unitOfWorkMock
+                .Setup(x => x.Clients.GetSingleAsync(It.IsAny<Expression<Func<Client, bool>>>(), It.IsAny<bool>(), null!))
+                .ReturnsAsync((Client?)null);
+
+            var handler = CreateHandler();
+
+            // Act
+            var result = await handler.Handle(command, CancellationToken.None);
+
+            // Assert
+            Assert.True(result.IsSuccess);
+            _unitOfWorkMock.Verify(x => x.Clients.AddAsync(It.IsAny<Client>()), Times.Once);
+            _unitOfWorkMock.Verify(x => x.CompleteAsync(), Times.Once);
+        }
+    }
+}
