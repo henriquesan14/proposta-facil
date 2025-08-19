d52a0cda6b786a20d2215f9355cf1539e2f3bb16
Henrique <henriqueunipe@gmail.com>
2025-08-19 01:41:33 -0300

    Get Users query filtros

--- src/PropostaFacil.API/Controllers/UserController.cs
+++ src/PropostaFacil.API/Controllers/UserController.cs
@@ -15,9 +15,8 @@ namespace PropostaFacil.API.Controllers
     public class UserController(IMediator mediator) : BaseController
     {
         [HttpGet]
-        public async Task<IActionResult> Get([FromQuery] PaginationRequest paginationRequest,CancellationToken ct)
+        public async Task<IActionResult> Get([FromQuery] GetUsersQuery query,CancellationToken ct)
         {
-            var query = new GetUsersQuery(paginationRequest);
             var result = await mediator.Send(query, ct);
 
             return result.Match(
--- src/PropostaFacil.Application/Users/Queries/GetUsers/GetUsersQuery.cs
+++ src/PropostaFacil.Application/Users/Queries/GetUsers/GetUsersQuery.cs
@@ -1,8 +1,9 @@
 ﻿using Common.ResultPattern;
+using PropostaFacil.Domain.Enums;
 using PropostaFacil.Shared.Common.CQRS;
 using PropostaFacil.Shared.Common.Pagination;
 
 namespace PropostaFacil.Application.Users.Queries.GetUsers
 {
-    public record GetUsersQuery(PaginationRequest PaginationRequest) : IQuery<ResultT<PaginatedResult<UserResponse>>>;
+    public record GetUsersQuery(Guid? TenantId, string? Name, UserRoleEnum? Role, int PageNumber = 1, int PageSize = 20) : IQuery<ResultT<PaginatedResult<UserResponse>>>;
 }
